96bbbc2412bc25f1a535892245b124bc86cdde8c
Plutone11011 <borelliplutone@gmail.com>
2022-11-10 10:44:18 +0100

    post user message

--- frontend/gui.py
+++ frontend/gui.py
@@ -12,7 +12,7 @@ class MainWindow:
         self.window.columnconfigure(0, weight=1)
         self.window.rowconfigure(0, weight=1) 
 
-        self.chat_area = Text(self.mainframe)
+        self.chat_area = Text(self.mainframe, padx=10, pady=10, borderwidth=5)
         self.chat_area.grid(column=2, row=2, padx=10, pady=20)
 
         self.input_area = Text(self.mainframe, height=3)
@@ -24,12 +24,30 @@ class MainWindow:
         first_msg = requests.get("http://127.0.0.1:5000")
 
         if first_msg.status_code == 200:
+            
+            self.chat_area.insert("1.0", ' '.join(first_msg.json()["data"].split()) + "\n\n") # splitting and joining to eliminate tabs and line breaks
 
-            self.chat_area.insert(1.0, ' '.join(first_msg.json()["data"].split())) # splitting and joining to eliminate tabs and line breaks
         
         self.chat_area["state"] = "disabled"
 
+        def post_user_message(e):
+
+            msg = self.input_area.get(1.0, END)
+            self.input_area.delete(1.0, END)
+            
+            self.chat_area["state"] = "normal"
+            self.chat_area.insert("end", msg)
+            self.chat_area["state"] = "disabled"
+        
+        self.input_area.bind('<Return>', post_user_message)
+
         self.window.mainloop()
+    
+    
+        
+    
 
 if __name__ == '__main__':
-    MainWindow()
+    main_window = MainWindow()
+
+    main_window.listen_user_input()
